ab76cd1a8b31217025bac2af18a796ce4411573e
Rashuneagle <mscotteagle21@gmail.com>
2024-06-07 20:54:50 -0500

    edited vite config and package to right version

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
   },
   server: {
-    host: '0.0.0.0',  // Bind to all network interfaces
-    port: 5173,       
-  }
+    host: '0.0.0.0',
+    port: 5173,
+  },
 });
